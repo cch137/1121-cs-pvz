8f4ea7c483735f397466378bc24460ffb4f9a638
cch137 <96408836+cch137@users.noreply.github.com>
2023-10-14 12:47:43 +0800

    sprite attributes

--- components/sprite.py
+++ components/sprite.py
@@ -1,7 +1,9 @@
 import pygame
 
 class Sprite(pygame.sprite.Sprite):
-    listeners: dict[str, set[function]] = {}
+    __attributes = {}
+    __listeners: dict[str, set[function]] = {}
+    
     bg_color: str = None
 
     def __init__(self, image: pygame.Surface | (int, int)):
@@ -15,23 +17,32 @@ class Sprite(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
 
     def addEventListener(self, event: str, listener: function):
-        if event not in self.listeners:
-            self.listeners[event] = set()
-        self.listeners[event].add(listener)
+        if event not in self.__listeners:
+            self.__listeners[event] = set()
+        self.__listeners[event].add(listener)
         eventManager.addSprite(self, event)
 
     def removeEventListener(self, event: str, listener: function):
-        if event not in self.listeners: return
-        if listener not in self.listeners[event]: return
-        self.listeners[event].remove(listener)
-        if self.listeners[event].__len__() == 0:
+        if event not in self.__listeners: return
+        if listener not in self.__listeners[event]: return
+        self.__listeners[event].remove(listener)
+        if self.__listeners[event].__len__() == 0:
             eventManager.removeSprite(self, event)
 
     def dispatchEvent(self, event: str):
-        if event not in self.listeners: return
-        for listener in self.listeners[event]:
+        if event not in self.__listeners: return
+        for listener in self.__listeners[event]:
             try: listener()
             except: pass
+    
+    def setAttribute(self, name: str, value):
+        self.__attributes[name] = value
+    
+    def rmvAttribute(self, name: str):
+        del self.__attributes[name]
+    
+    def hasAttribute(self, name: str):
+        return name in self.__attributes
 
 class Grid(Sprite):
     pass
