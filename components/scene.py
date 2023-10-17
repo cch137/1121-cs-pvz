7c88149cb81b1f7d47860b3809f426945d021b1b
cch137 <96408836+cch137@users.noreply.github.com>
2023-10-18 01:02:47 +0800

    feat: get_element_by_id

--- components/scene.py
+++ components/scene.py
@@ -2,4 +2,10 @@ class Scene(): pass
 
 from components.element import *
 
-class Scene(): pass
+class Scene():
+    all_elements: set[Element] = set()
+
+    def get_element_by_id(self, id: str):
+        for element in self.all_elements:
+            if element.id == id:
+                return element
