d8ccf41b3ef62bbdf91f6041aa072b3bf2893095
cch137 <96408836+cch137@users.noreply.github.com>
2023-10-18 11:00:59 +0800

    Scene

--- components/scene.py
+++ components/scene.py
@@ -1,11 +1,22 @@
+import pygame
+
 class Scene(): pass
 
 from components.element import *
 
 class Scene():
-    all_elements: set[Element] = set()
+    def __init__(self, screen: pygame.Surface):
+        self.screen = screen
+
+    __elements: set[Element] = set()
 
     def get_element_by_id(self, id: str):
-        for element in self.all_elements:
+        for element in self.__elements:
             if element.id == id:
                 return element
+    
+    def add_element(self, element: Element):
+        self.__elements.add(element)
+    
+    def remove_element(self, element: Element):
+        self.__elements.remove(element)
