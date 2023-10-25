d002eae4dffc52f083b0fa1b6d6551e1deafdd6e
cch137 <96408836+cch137@users.noreply.github.com>
2023-10-25 23:26:52 +0800

    fix: add imports

--- components/scenes/main_game.py
+++ components/scenes/main_game.py
@@ -1,4 +1,5 @@
 from constants import *
+import components.events as events
 from components.scenes import Scene
 from components.element import Element
 from components.controller import controller
--- components/scenes/testing1.py
+++ components/scenes/testing1.py
@@ -1,6 +1,40 @@
 from constants import *
+import components.events as events
 from components.scenes import Scene
 from components.element import Element
 from components.controller import controller
 
+def make_color_block(color: (255, 255, 255)):
+    ele = Element((50, 50))
+    ele.background_color = color
+    return ele
+
+children = [make_color_block(c) for c in [
+    (255, 0, 0), (0, 255, 0), (0, 0, 255)
+]]
+
+parent_ele = Element()
+
+parent_ele.x = 50
+parent_ele.y = 50
+
+parent_ele.append_child(*children)
+
+parent_ele.min_height = 200
+parent_ele.min_width = 150
+parent_ele.spacing = 25
+
+def blue_clicked():
+    print('blue clicked')
+
+children[2].add_event_listener(events.CLICK, blue_clicked)
+
+children[0].cursor = 'crosshair'
+children[1].cursor = 'sizeall'
+children[2].cursor = 'hand'
+
 tesing1 = Scene()
+
+tesing1.add_element(parent_ele)
+
+controller.goto_scene(tesing1)
