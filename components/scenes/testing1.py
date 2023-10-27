f01a16075a9ba7fbbcef9ffdd3de24856be5aa93
cch137 <96408836+cch137@users.noreply.github.com>
2023-10-27 15:00:50 +0800

    add coments

--- components/scenes/testing1.py
+++ components/scenes/testing1.py
@@ -9,49 +9,64 @@ from components.controller import controller
 testing1 = scenes.Scene()
 
 def init():
+    # 一個製作 element 的函式
     def make_color_block(color: (255, 255, 255)):
         ele = element.Element((50, 50))
         ele.background_color = color
         return ele
 
+    # 根據給定數據（rgb 值）製作一個 list 的 element
     children = [make_color_block(c) for c in [
         (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0)
     ]]
 
+    # 設置 parent_ele 的樣式
     parent_ele = element.Element()
     parent_ele.min_width = 150
     parent_ele.max_height = 400
     parent_ele.x = 50
     parent_ele.y = 50
     parent_ele.spacing = 25
+    # 將 children 添加到 parent_ele
     parent_ele.append_child(*children)
 
-    def blue_clicked():
-        print('blue clicked')
-
+    # 設置鼠標在元素上的樣式
     children[0].cursor = 'crosshair'
     children[1].cursor = 'sizeall'
     children[2].cursor = 'ibeam'
     children[3].cursor = 'hand'
 
+    # 添加 click 事件監聽器
+    def blue_clicked():
+        print('blue clicked')
     children[2].add_event_listener(events.CLICK, blue_clicked)
 
-    # image elements
+    # 創建 image_ele1
     image_ele1 = element.Element(element.load_image('icon.png', (50, 50)))
     image_ele1.cursor = 'hand'
+
+    # 添加 click 事件監聽器
     def image_ele_clicked():
         image_ele1.image = pygame.transform.flip(image_ele1.image, True, False)
     image_ele1.add_event_listener(events.CLICK, image_ele_clicked)
 
+    # 創建 image_ele2
     image_ele2 = element.Element(element.load_image('entities/sun.png', (60, 60)))
 
+    # 將 image_ele1, image_ele2 添加到 parent_ele
     parent_ele.append_child(image_ele1, image_ele2)
 
+    # 重新添加 children[3] 到 parent_ele
+    # - 這會導致 children[3] 從 parent_ele 中移除
+    # - 然後 children[3] 會重新被添加到 parent_ele.childern 的尾部
     parent_ele.append_child(children[3])
+
+    # 添加 click 事件監聽器（頁面轉跳）
     def goto_testing2():
         controller.goto_scene(controller.scenes.testing2)
     children[3].add_event_listener(events.CLICK, goto_testing2)
 
+    # 將 parent_ele 添加到 testing1 場景
     testing1.add_element(parent_ele)
 
 testing1.init = init
--- components/scenes/testing2.py
+++ components/scenes/testing2.py
@@ -9,16 +9,19 @@ from components.controller import controller
 testing2 = scenes.Scene()
 
 def init():
+    # 創建 outer_element
     outer_element = element.Element()
     outer_element.min_width = WINDOW_WIDTH
     outer_element.min_height = WINDOW_HEIGHT
 
+    # 創建 inner_element
     inner_element = element.Element((600, 400))
     inner_element.min_width = 600
     inner_element.min_height = 400
     inner_element.background_color = (255, 0, 0)
     inner_element.spacing = 10
 
+    # 創建 btn1, btn2, btn3
     btn1 = element.Element((300, 80))
     btn1.background_color = (0, 255, 255)
     btn2 = element.Element((300, 80))
@@ -27,6 +30,7 @@ def init():
     btn3.background_color = (0, 255, 0)
     btn3.spacing = 15
 
+    # 創建 btn3_1, btn3_2, btn3_3
     btn3_1 = element.Element((75, 50))
     btn3_1.background_color = (0, 0, 255)
     btn3_2 = element.Element((75, 50))
@@ -34,15 +38,18 @@ def init():
     btn3_3 = element.Element((75, 50))
     btn3_3.background_color = (0, 0, 255)
 
+    # 添加 click 事件監聽器（頁面轉跳）
     def goto_testing1():
         controller.goto_scene(controller.scenes.testing1)
     btn1.add_event_listener(events.CLICK, goto_testing1)
     btn1.cursor = 'hand'
 
+    # 把 elements 添加到父級元素
     outer_element.append_child(inner_element)
     inner_element.append_child(btn1, btn2, btn3)
     btn3.append_child(btn3_1, btn3_2, btn3_3)
 
+    # 把 outer_element 添加到 testing2 場景
     testing2.add_element(outer_element)
 
 testing2.init = init
