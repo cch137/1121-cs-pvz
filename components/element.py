beb2416f415fedf2eaf793f0d9b9bf8103b2d6e5
cch137 <96408836+cch137@users.noreply.github.com>
2023-10-28 18:16:17 +0800

    fix: better

--- components/element.py
+++ components/element.py
@@ -122,7 +122,7 @@ class Element(pygame.sprite.Sprite, events.EventTarget):
     def children(self):
         return list(self.__children)
 
-    def __init__(self, image: pygame.Surface | Tuple[int,int] | None = None, display: Literal['block', 'inline', 'row', 'column'] = BLOCK, children: List[Element] = []):
+    def __init__(self, image: pygame.Surface | Tuple[int,int] | None = None, display: Literal['block', 'inline', 'row', 'column'] | None = None, children: List[Element] = []):
         '''使用大小創建 Element 會將該大小設為此 Element 的 min_width 和 min_height'''
         pygame.sprite.Sprite.__init__(self)
         events.EventTarget.__init__(self)
@@ -135,7 +135,7 @@ class Element(pygame.sprite.Sprite, events.EventTarget):
             self.min_width, self.min_height = image
         else:
             raise 'Invalid image'
-        self.display = display
+        self.display = display or BLOCK
         self.rect = self.image.get_rect()
         self.__children = list()
         self.caches = CacheManager()
