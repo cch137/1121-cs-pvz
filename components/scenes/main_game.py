8e639838f0fbdfe9092cd9aa60c102142d42388d
cch137 <96408836+cch137@users.noreply.github.com>
2023-10-29 12:51:09 +0800

    testing

--- components/scenes/main_game.py
+++ components/scenes/main_game.py
@@ -8,7 +8,7 @@ main_game = scenes.Scene()
 def init():
     from components import Element, load_image, load_animation, create_textbox, \
         events, Entity, Plant, Zombie, controller
-    # wangcc
+    # testing 123
     pass
 
 main_game.init = init
