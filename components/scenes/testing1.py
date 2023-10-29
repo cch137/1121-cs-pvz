a63c79d8c16a16832a0a80be56f55ecce0a891f3
cch137 <96408836+cch137@users.noreply.github.com>
2023-10-29 08:53:16 +0800

    add comments

--- components/scenes/testing1.py
+++ components/scenes/testing1.py
@@ -20,15 +20,10 @@ def init():
         (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0)
     ]]
 
-    # 設置 parent_ele 的樣式
-    parent_ele = Element()
-    parent_ele.min_width = 150
-    parent_ele.max_height = 400
-    parent_ele.x = 50
-    parent_ele.y = 50
-    parent_ele.spacing = 25
-    # 將 children 添加到 parent_ele
-    parent_ele.append_child(*children)
+    # 添加 click 事件監聽器
+    children[2].add_event_listener(events.CLICK, lambda: print('blue clicked'))
+    # 添加 click 事件監聽器（頁面轉跳）
+    children[3].add_event_listener(events.CLICK, lambda: controller.goto_scene(controller.scenes.testing2))
 
     # 設置鼠標在元素上的樣式
     children[0].cursor = 'crosshair'
@@ -36,67 +31,66 @@ def init():
     children[2].cursor = 'ibeam'
     children[3].cursor = 'hand'
 
-    # 添加 click 事件監聽器
-    def blue_clicked():
-        print('blue clicked')
-    children[2].add_event_listener(events.CLICK, blue_clicked)
+    # 創建 parent_ele 並設置樣式
+    parent_ele = Element()
+    parent_ele.min_width = 150
+    parent_ele.min_height = 400
+    parent_ele.x = 50
+    parent_ele.y = 50
+    parent_ele.spacing = 25
 
-    # 創建 image_ele1
-    image_ele1 = Element(load_image('icon.png', (50, 50)))
-    image_ele1.cursor = 'hand'
+    # 將 children 添加到 parent_ele
+    parent_ele.append_child(*children)
+
+    # 創建 troll_face
+    troll_face = Element(load_image('icon.png', (50, 50)))
+    troll_face.cursor = 'hand'
 
     # 添加 click 事件監聽器
     def image_ele_clicked():
-        image_ele1.image = pygame.transform.flip(image_ele1.image, True, False)
-    image_ele1.add_event_listener(events.CLICK, image_ele_clicked)
+        troll_face.image = pygame.transform.flip(troll_face.image, True, False)
+    troll_face.add_event_listener(events.CLICK, image_ele_clicked)
 
-    # 創建 image_ele2
-    image_ele2 = Entity(load_image('entities/sun.png', (60, 60)))
+    # 創建 demo_plant 並設置其屬性
+    demo_plant = Plant(load_image('plants/demo.png', (128, 128)))
+    demo_plant.image = pygame.transform.flip(demo_plant.image, True, False)
+    demo_plant.rect.right = controller.screen_rect.right - 10
+    demo_plant.rect.centery = controller.screen_rect.centery
+    demo_plant.cursor = 'hand'
 
-    # 創建 image_ele3
-    image_ele3 = Plant(load_image('plants/demo.png', (128, 128)))
-    image_ele3.image = pygame.transform.flip(image_ele3.image, True, False)
-    image_ele3.rect.right = controller.screen_rect.right - 10
-    image_ele3.rect.centery = controller.screen_rect.centery
-    image_ele3.cursor = 'hand'
+    # 創建子彈
     def plant_demo_shoot():
         bullet = Entity((10, 10))
         bullet.allow_flyout = False
         bullet.background_color = (255, 0, 255)
         bullet.velocity_x = -10
-        bullet.rect.centerx = image_ele3.rect.centerx - 32
-        bullet.rect.centery = image_ele3.rect.centery - 18
+        bullet.rect.centerx = demo_plant.rect.centerx - 32
+        bullet.rect.centery = demo_plant.rect.centery - 18
         bullet.collision_damage = 10
         bullet.collision_target_types.add(Entity)
         bullet.z_index = 99
         testing1.add_element(bullet)
-    image_ele3.add_event_listener(events.CLICK, plant_demo_shoot)
-
-    # 將 image_ele1, image_ele2 添加到 parent_ele
-    parent_ele.append_child(image_ele1, image_ele2)
+    demo_plant.add_event_listener(events.CLICK, plant_demo_shoot)
 
     # 列印當前場景元素總數
     # parent_ele.update = lambda: print(tuple(testing1.elements_generator).__len__())
+
+    # 將元素添加到 parent_ele
+    parent_ele.append_child(troll_face)
 
     # 重新添加 children[3] 到 parent_ele
     # - 這會導致 children[3] 從 parent_ele 中移除
     # - 然後 children[3] 會重新被添加到 parent_ele.childern 的尾部
     parent_ele.append_child(children[3])
 
-    # 添加 click 事件監聽器（頁面轉跳）
-    def goto_testing2():
-        controller.goto_scene(controller.scenes.testing2)
-    children[3].add_event_listener(events.CLICK, goto_testing2)
-
-    # 將 parent_ele 添加到 testing1 場景
+    # 將 Element 添加到場景
     testing1.add_element(parent_ele)
-
-    # 將 image_ele3 添加到 testing1 場景
-    testing1.add_element(image_ele3)
+    testing1.add_element(demo_plant)
 
     # 創建導航
-    navigator = Element(None, None)
+    navigator = Element()
     navigator.spacing = 16
+
     # 創建轉跳按鈕
     def create_link(scene_name: str, scene: scenes.Scene):
         element = Element(create_textbox(scene_name, 24))
@@ -115,6 +109,7 @@ def init():
         ('pause_menu', scenes.pause_menu),
         ('the_end', scenes.the_end),
     ): create_link(scene_name, scene)
+
     # 注意！透過 .rect 設置坐標的時候，先將 Element 添加到場景
     # 然後使用 Scene.compose() 進行排版，最後才設置坐標
     # 這是為了防止坐標設置失敗（子元素尚未加載所導致的）
