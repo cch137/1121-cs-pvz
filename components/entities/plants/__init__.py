bdd53fb2da4b3a2e40a404ce44e532bd90fd4484
cch137 <96408836+cch137@users.noreply.github.com>
2023-11-01 16:29:13 +0800

    增加 all_plants 和 all_zombies 集合

--- components/entities/plants/__init__.py
+++ components/entities/plants/__init__.py
@@ -4,11 +4,14 @@ from components.entities import Entity
 
 class Plant: pass
 
+all_plants: set[Plant] = set()
+
 import components.entities.zombies as zombies
 
 class Plant(Entity):
     def __init__(self, image: pygame.Surface):
         Entity.__init__(self, image)
+        all_plants.add(self)
     
     @property
     def closest_zombie(self) -> zombies.Zombie:
@@ -17,3 +20,9 @@ class Plant(Entity):
     @property
     def has_seen_plants(self) -> bool:
         return
+
+    def kill(self, *args: Any, **kargs):
+        Entity.kill(self, *args, **kargs)
+        all_plants.remove(self)
+
+from components.entities.zombies import all_zombies
--- components/entities/zombies/__init__.py
+++ components/entities/zombies/__init__.py
@@ -4,11 +4,14 @@ from components.entities import Entity
 
 class Zombie: pass
 
+all_zombies: set[Zombie] = set()
+
 import components.entities.plants as plants
 
 class Zombie(Entity):
     def __init__(self, image: pygame.Surface):
         Entity.__init__(self, image)
+        all_zombies.add(self)
     
     @property
     def closest_plant(self) -> plants.Plant:
@@ -17,3 +20,9 @@ class Zombie(Entity):
     @property
     def has_seen_zombies(self) -> bool:
         return
+
+    def kill(self, *args: Any, **kargs):
+        Entity.kill(self, *args, **kargs)
+        all_zombies.remove(self)
+
+from components.entities.plants import all_plants
