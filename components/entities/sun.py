39c583fd39f034f9be6eff468eef582c04059448
cch137 <96408836+cch137@users.noreply.github.com>
2023-11-10 15:28:33 +0800

    adjust size

--- components/entities/sun.py
+++ components/entities/sun.py
@@ -2,12 +2,14 @@ from components.media import media
 from components.entities import Entity
 from random import randint
 
+SUN_SIZE = 75
+
 class Sun(Entity):
     def __init__(self, value=25, x_range: tuple[int, int] = (100, 980), y_range: tuple[int, int] = (200, 700)):
         self.value = value
-        Entity.__init__(self, media.load_image('entities/sun.png', (75, 75)))
-        self.rect.center = (randint(*x_range), -60)
-        self.move_limit = int(60 / 2 + randint(*y_range))
+        Entity.__init__(self, media.load_image('entities/sun.png', (SUN_SIZE, SUN_SIZE)))
+        self.rect.center = (randint(*x_range), -SUN_SIZE)
+        self.move_limit = int(SUN_SIZE / 2 + randint(*y_range))
         self.velocity_y = 3
         self.z_index = 999
         self.cursor = 'hand'
