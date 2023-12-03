425343cd866b84c9c32d047c7b8167ed18027033
cch137 <96408836+cch137@users.noreply.github.com>
2023-12-03 16:20:16 +0800

    press any key 提示

--- components/scenes/main_menu.py
+++ components/scenes/main_menu.py
@@ -16,12 +16,15 @@ def init():
 
     start_button = TextBox('CLICK TO START')
     start_button.rect.center = (WINDOW_WIDTH/2, 600)
-    main_menu.add_element(start_button)
     start_button.add_event_listener(events.KEYDOWN, lambda: controller.goto_scene(controller.scenes.main_game))
     start_button.add_event_listener(events.CLICK, lambda: controller.goto_scene(controller.scenes.main_game))
+    start_button.cursor = 'hand'
+    main_menu.add_element(start_button)
 
-
-
+    press_any_key_hint = TextBox('press any key to start', 18, (128, 128, 128))
+    press_any_key_hint.rect.centerx = WINDOW_WIDTH / 2
+    press_any_key_hint.rect.bottom = WINDOW_HEIGHT / 2 + 160
+    main_menu.add_element(press_any_key_hint)
 
 main_menu.init = init
 
