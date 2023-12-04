e7a315869871afad4fb147ed99115e259dcd0352
cch137 <96408836+cch137@users.noreply.github.com>
2023-12-04 15:05:50 +0800

    adjust sun size

--- components/entities/sun.py
+++ components/entities/sun.py
@@ -6,7 +6,7 @@ from typing import Tuple, Any
 class Sun(Entity):
     def __init__(self, x_range: Tuple[int, int], y_range: Tuple[int, int], value=25):
         self.value = value
-        Entity.__init__(self, media.load_image('entities/sun.png', 75))
+        Entity.__init__(self, media.load_image('entities/sun.png', 80))
         self.rect.center = (randint(*x_range), -self.rect.height / 2)
         self.move_limit = int(self.rect.height / 2 + randint(*y_range))
         self.velocity_y = 3
