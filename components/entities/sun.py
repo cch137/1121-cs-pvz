52ceba1c1b2ad48deba204346e9d8735dd79d3ae
cch137 <96408836+cch137@users.noreply.github.com>
2023-12-04 23:05:26 +0800

    restruct

--- components/entities/sun.py
+++ components/entities/sun.py
@@ -2,20 +2,22 @@ from components.media import media
 from components.entities import Entity
 from random import randint
 from typing import Tuple, Any
+import components.events as events
 
 class Sun(Entity):
-    def __init__(self, x_range: Tuple[int, int], y_range: Tuple[int, int], value=25):
+    def __init__(self, value=25):
         self.value = value
         Entity.__init__(self, media.load_image('demo/Sun_0.png', 80))
-        self.rect.center = (randint(*x_range), -self.rect.height / 2)
-        self.move_limit = int(self.rect.height / 2 + randint(*y_range))
         self.velocity_y = 3
         self.velocity_a = 1 * (1 if randint(0, 1) else -1)
         self.z_index = 999
         self.radius_scale = 0.6
         self.cursor_r = 'hand'
-    
+        self.add_event_listener(events.CLICK_R, lambda: controller.level.add_suns(self.kill()))
+
     def kill(self, *args: Any, **kwargs: Any):
         '''Returns sun value.'''
         Entity.kill(self, *args, **kwargs)
         return self.value
+
+from components import controller
