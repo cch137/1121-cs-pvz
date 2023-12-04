f20483335cbd39352321432a81414c0ecfa81240
cch137 <96408836+cch137@users.noreply.github.com>
2023-12-04 23:01:30 +0800

    debug Plants

--- components/entities/plants/SnowPea.py
+++ components/entities/plants/SnowPea.py
@@ -2,9 +2,17 @@ from utils.constants import *
 from components.entities.plants import Shooter, BulletTemplate
 from components.media import media
 from components.entities.zombies import Zombie
-from components.entities.__init__ import SlowDownEffect
+from components.entities import SlowDownEffect
 
-    
+pea_ice_template = BulletTemplate(
+    media.load_image('demo/PeaIce_0.png', BULLET_SIZE) , 
+    BULLET_SPEED,
+    None,
+    15,
+    [Zombie],
+    [SlowDownEffect('SnowPea', 180, 0.3)]
+)
+
 class SnowPea(Shooter):
     def __init__(self):
         Shooter.__init__(
@@ -12,14 +20,7 @@ class SnowPea(Shooter):
             media.load_image('demo/SnowPea_0.png', PLANT_SIZE),
             175,
             (0.5, 0.5),
-            BulletTemplate(
-                media.load_image('demo/PeaIce_0.png', BULLET_SIZE) , 
-                BULLET_SPEED,
-                None,
-                15,
-                [Zombie]
-                [SlowDownEffect('SnowPea', 120, 0.3)]
-            ),
+            pea_ice_template,
             60,
         )
         self.health = 75
